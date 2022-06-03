2e175c7a3c5983b947badd825b445a53e1cccf79
Zachary Riggs <zach_riggs@yahoo.com>
2022-06-03 14:14:28 -0500

    zr-log-teammates

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("team Z and Friends")
+const teammatesNames = () => {
+    console.log("Zachary R")
+    console.log("Brittany M")
+    console.log("Mason T")
+    console.log("Zachary S")
+    console.log("Andrew W")
+}
