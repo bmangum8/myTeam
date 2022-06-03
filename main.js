1b35b97344bfc836f2c1a0a8eaed1db47b0d0f08
Zachary Riggs <zach_riggs@yahoo.com>
2022-06-03 10:55:04 -0500

    tammates names

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("team Z and Friends")
+const teammatesNames = () => {
+    console.log("Mason T")
+    console.log("Andrew W")
+    console.log("Brittany M")
+    console.log("Zach S")
+    console.log("Zach R")
+}
