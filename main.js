06e4703db23c6b46c86aea42eda4c10050450116
Mason Thieman <masonthieman@gmail.com>
2022-06-03 10:42:49 -0500

    add teammate colors

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("team Z and Friends")
+
+const teamColors = () => {
+    console.log("Mason's favorite color is blue")
+    console.log("Zach Riggs's favorite color is green")
+    console.log("Zach Smith's favorite color is yellow")
+    console.log("Brittany's favorite color is blue")
+    console.log("Andrew's favorite color is orange")
+}
