60600f2005909163ca7351c953ab82128eaba6dc
Andrew Ward <redrawdawn@gmail.com>
2022-06-03 10:39:54 -0500

    added everyones pets... poor mason

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("team Z and Friends")
+
+const teamPets = () => {
+    console.log("Mason is sad and lonely")
+    console.log("Andrew has a dog named Oreo")
+    console.log("Zach S has a cat named Emilia")
+    console.log("Zach R dog named Viola")
+    console.log("Brittany has 2 cats named Humo and Pepper")
+}
